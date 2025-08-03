6708fde048fede700eb8db8e80ac2416543813ae
THEosusi <bp20129@shibaura-it.ac.jp>
2025-08-03 11:27:44 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,68 @@
+import requests
+import json
+import time 
+import itertools
+import string
+
+url_template = 'https://api.github.com/search/code?q=%22istio.io%22%20%22kind%3A%20AuthorizationPolicy%22+language:YAML+filename:{name}&page={page}&per_page=100'
+headers = {
+  'Authorization': 'Token put your token'
+}
+characters = string.ascii_lowercase + string.digits + "-"
+limited_characters = string.ascii_lowercase + string.digits
+
+successful_responses = 0
+
+# Loop over the first character (char1)
+for char1 in limited_characters:
+    full_names_exclude = set()  # Reset for each char1
+    print(f"Processing char1: {char1}")
+
+    # Loop over the second character (char2)
+    for char2 in characters:
+        filename = f"{char1}{char2}"
+        print(f"Checking filename: {filename}")
+
+        for page in range(1, 11):
+            url = url_template.format(name=filename, page=page)
+            response = requests.get(url, headers=headers)
+
+            if response.status_code == 200:
+                data = response.json()
+                items = data.get("items", [])
+
+                if not items:
+                    print(f"No more results for this filename, stopping at page {page}.")
+                    break
+
+                for item in items:
+                    repo_name = item["repository"]["full_name"]
+                    full_names_exclude.add(repo_name)
+            else:
+                successful_responses += 1
+                print(f"Failed to fetch data for filename {filename}, page {page}, {response.headers} ,{response} url{url}")
+                break
+            time.sleep(10)
+        time.sleep(11)
+
+        if successful_responses >= 6:
+            print("Reached failure threshold (6), stopping execution.")
+            break
+
+
+    output_data = {
+        "repositories": list(full_names_exclude)
+    }
+
+    output_filename = f"Authori_{char1}.json"
+    with open(output_filename, "w", encoding="utf-8") as f:
+        json.dump(output_data, f, indent=2, ensure_ascii=False)
+
+    print(f"Unique Full Names count for {char1}: {len(full_names_exclude)}")
+    print(f"Results saved to {output_filename}")
+
+
+    if successful_responses >= 6:
+        break
+
+    time.sleep(7)
